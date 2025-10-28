4efbd7f4458fed4d93220f98d05fd22320b896cb
Menna Mohamed <mennamokhtar621@gmail.com>
2025-10-28 09:14:27 +0300

    Adusting_AccountController

---
+++ Pharmacom.Domain/ViewModels/LogInViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PharmaCom.Domain.ViewModels
+{
+    public class LogInViewModel
+    {
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "User Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ Pharmacom.Domain/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PharmaCom.Domain.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+
+        [Display(Name = "Address Line 1")]
+        public string Line1 { get; set; }
+
+        public string City { get; set; }
+
+        public string Governorate { get; set; }
+
+    }
+}
